dd9f17b920db00f82aa1b3269a968b33769d55ff
Uni- <administrator@korea.ac.kr>
2017-01-10 18:09:54 +0900

    Checkin 20170110: Raycast system test

---
+++ Assets/Scripts/InputHandResponse.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandResponse : MonoBehaviour
+{
+    private Camera mainCamera;
+    private GameObject sceneDynamicContainer;
+
+    [SerializeField]
+    Material WhiteShader;
+    [SerializeField]
+    Material RedShader;
+
+    // Use this for initialization
+    void Start()
+    {
+        mainCamera = Camera.main;
+        Camera[] allCameras = Camera.allCameras;
+
+        sceneDynamicContainer = GameObject.Find("SceneDynamic");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        List<Vector3> relativeInputCoordinates = new List<Vector3>();
+
+        for (int i = 0; i < 3; i++)
+        {
+            bool mouse = Input.GetMouseButton(i);
+            Vector3 position = Input.mousePosition;
+            if (mouse)
+            {
+                relativeInputCoordinates.Add(position);
+                break;
+            }
+        }
+
+        for (int i = 0; i < Input.touchCount; i++)
+        {
+            Touch touch = Input.GetTouch(i);
+            relativeInputCoordinates.Add(new Vector3(touch.position.x, touch.position.y));
+            Debug.Log(touch);
+        }
+
+        for (int i = 0; i < sceneDynamicContainer.transform.childCount; i++)
+        {
+            GameObject gameObject = sceneDynamicContainer.transform.GetChild(i).gameObject;
+        }
+
+        foreach (Vector3 vectorItem in relativeInputCoordinates)
+        {
+            Ray ray = mainCamera.ScreenPointToRay(vectorItem);
+            ray.origin = mainCamera.transform.position;
+            RaycastHit raycastHit;
+            bool hitObjectExists = Physics.Raycast(ray.origin, ray.direction, out raycastHit);
+            if (hitObjectExists)
+            {
+                raycastHit.transform.gameObject.GetComponent<Renderer>().material = RedShader;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ObjectBubblePop.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectBubblePop : MonoBehaviour
+{
+    private float accDeltaTick;
+    private float nextGenTick;
+
+    [SerializeField]
+    GameObject PopBubble;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
