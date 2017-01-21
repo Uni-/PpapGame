06f2ba1aa95f8600077d882977a6aef93bd1a6bf
Uni- <administrator@korea.ac.kr>
2017-01-21 10:58:57 +0900

    Move Csharp scripts from Assets/Scripts/ to .../Model and .../UnityMonoBehaviour

---
+++ Assets/Scripts/Model/HandItemEventArgs.cs
@@ -0,0 +1,11 @@
+﻿public struct HandItemEventArgs
+{
+    public HandItemEventType eventType;
+
+    public HandType handType;
+    public string entered;
+
+    public string LhT;
+    public string RhT;
+    public string Purged;
+}
--- Assets/Scripts/UnityMonoBehaviour/StateMgmtHand.cs
+++ Assets/Scripts/UnityMonoBehaviour/StateMgmtHand.cs
@@ -1,19 +1,6 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public struct HandItemEventArgs
-{
-    public HandItemEventType eventType;
-
-    public HandType handType;
-    public string entered;
-
-    public string LhT;
-    public string RhT;
-    public string Purged;
-}
 
 public class StateMgmtHand : MonoBehaviour
 {
